40409dbe993bd74d8385668195a50633342213f4
Tom Rafferty <86563641+TomRafferty@users.noreply.github.com>
2022-07-25 17:03:12 +0100

    in-progress changes - print changes
    making some changes to how the print statements are done so the console is in a more readable state.
    
    Note: For better practice these should be moved into their own class so the methods may be called in any other classes -> in this case it's not needed but would be a good idea for real projects.

--- src/test/scala/Agenda.scala
+++ src/test/scala/Agenda.scala
@@ -5,24 +5,42 @@ class Agenda (val meetings: List[Meeting]) {
     val meetingsAM = meetingsForTheDay.filter(meeting => meeting.time.contains("am"))
     val meetingsPM = meetingsForTheDay.filter(meeting => meeting.time.contains("pm"))
 
+    def printLineSmall: Unit = println("-------------")
+    def printLineLarge: Unit = println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    def printHead(str: String): Unit = {
+      print(Console.GREEN)
+      print(Console.BOLD)
+      print(Console.UNDERLINED)
+      println(str)
+      printLineSmall
+      print(Console.RESET)
+    }
+    def printSub(str: String): Unit = {
+      print(Console.BLUE)
+      print(Console.UNDERLINED)
+      println(str)
+      print(Console.RESET)
+    }
+
     def meetingPrinter(meetings: List[Meeting]): Unit = {
       meetings.foreach(meeting => {
         println(s"${meeting.time}: ${meeting.name}")
       })
     }
-    println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
-    println(s"$day:")
+
+    printLineLarge
+    printHead(s"$day:")
     if(meetingsForTheDay.length > 0){
       if(meetingsAM.length > 0){
-        println("AM Meetings:")
-        println("-------------")
+        printSub("AM Meetings:")
+        printLineSmall
         meetingPrinter(meetingsAM)
       }else{
         println("There are no AM meetings scheduled")
       }
       if(meetingsPM.length > 0){
-        println("PM Meetings:")
-        println("-------------")
+        printSub("PM Meetings:")
+        printLineSmall
         meetingPrinter(meetingsPM)
       }else{
         println("There are no PM meetings scheduled")
@@ -30,7 +48,8 @@ class Agenda (val meetings: List[Meeting]) {
     }else{
       println(s"No meetings for $day")
     }
-    println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+
+    printLineLarge
     println()
   }
 }
